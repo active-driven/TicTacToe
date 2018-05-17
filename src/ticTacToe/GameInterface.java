563e8a0a4470b88240da20507d1b29636ed11027
dream-tree <marcinklimek@yahoo.com>
2018-05-17 16:37:43 +0200

    spacing adjustments

--- src/ticTacToe/GameInterface.java
+++ src/ticTacToe/GameInterface.java
@@ -27,119 +27,119 @@ public class GameInterface extends Application {
 
 		PlayGame test = new PlayGame();
 		
-        primaryStage.setTitle("Tic Tac Toe");
+		primaryStage.setTitle("Tic Tac Toe");
 
-        GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(1);
-        grid.setVgap(1);
-        grid.setPadding(new Insets(35, 35, 35, 35));
-        grid.setStyle("-fx-background-color: transparent;");   // or null; if not set, scene background is (default) white always
-    //  grid.setGridLinesVisible(true);
+		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.CENTER);
+		grid.setHgap(1);
+		grid.setVgap(1);
+		grid.setPadding(new Insets(35, 35, 35, 35));
+		grid.setStyle("-fx-background-color: transparent;");   // or null; if not set, scene background is (default) white always
+	//	grid.setGridLinesVisible(true);
 
         
-        Text startingLine = new Text("Click me:)");
-        startingLine.setFill(Color.BLACK);
-        startingLine.setFont(Font.font("Tahoma", FontWeight.EXTRA_BOLD, 20));
-        grid.add(startingLine, 0, 0, 4, 1);
-        Label emptyLine = new Label("");
-        grid.add(emptyLine, 0, 1, 4, 1);
+		Text startingLine = new Text("Click me:)");
+		startingLine.setFill(Color.BLACK);
+		startingLine.setFont(Font.font("Tahoma", FontWeight.EXTRA_BOLD, 20));
+		grid.add(startingLine, 0, 0, 4, 1);
+		Label emptyLine = new Label("");
+		grid.add(emptyLine, 0, 1, 4, 1);
         
-        setButtons(grid, test);
+		setButtons(grid, test);
                    
-        Label gameInfo = new Label("Game info: ");
-        gameInfo.setFont(Font.font("Tahoma", FontWeight.BOLD, 20));
-        grid.add(gameInfo, 1, 30, 2, 30);
+		Label gameInfo = new Label("Game info: ");
+		gameInfo.setFont(Font.font("Tahoma", FontWeight.BOLD, 20));
+		grid.add(gameInfo, 1, 30, 2, 30);
       
-        gameStatus = new Text("Player 1 starts the game");
-        gameStatus.setFill(Color.WHITESMOKE);
-        gameStatus.setFont(Font.font("Tahoma", FontWeight.BOLD, 20));
-        grid.add(gameStatus, 2, 30, 4, 30);   
+		gameStatus = new Text("Player 1 starts the game");
+		gameStatus.setFill(Color.WHITESMOKE);
+		gameStatus.setFont(Font.font("Tahoma", FontWeight.BOLD, 20));
+		grid.add(gameStatus, 2, 30, 4, 30);   
         
-        Label gameName = new Label("TicTacToe");
-        gameName.setFont(Font.font(STYLESHEET_CASPIAN, FontPosture.ITALIC, 14));
-        gameName.setLayoutX(-5);
-        Label gameVersion = new Label("ver. 1.0");
-        gameVersion.setFont(Font.font(STYLESHEET_CASPIAN, FontPosture.ITALIC, 14));
-        gameVersion.setAlignment(Pos.BASELINE_RIGHT);
-        Label authorName = new Label("by dream_tree");
-        authorName.setFont(Font.font(STYLESHEET_CASPIAN, FontPosture.ITALIC, 14));
-        authorName.setTextFill(Color.DARKORCHID);
-        grid.add(gameName, 3, 70);
-        grid.add(gameVersion, 3, 71);
-        grid.add(authorName, 3, 72);
+		Label gameName = new Label("TicTacToe");
+		gameName.setFont(Font.font(STYLESHEET_CASPIAN, FontPosture.ITALIC, 14));
+		gameName.setLayoutX(-5);
+		Label gameVersion = new Label("ver. 1.0");
+		gameVersion.setFont(Font.font(STYLESHEET_CASPIAN, FontPosture.ITALIC, 14));
+		gameVersion.setAlignment(Pos.BASELINE_RIGHT);
+		Label authorName = new Label("by dream_tree");
+		authorName.setFont(Font.font(STYLESHEET_CASPIAN, FontPosture.ITALIC, 14));
+		authorName.setTextFill(Color.DARKORCHID);
+		grid.add(gameName, 3, 70);
+		grid.add(gameVersion, 3, 71);
+		grid.add(authorName, 3, 72);
               
-        Scene scene = new Scene(grid, 600, 600);
-        scene.setFill(Color.YELLOWGREEN); 
+		Scene scene = new Scene(grid, 600, 600);
+		scene.setFill(Color.YELLOWGREEN); 
         
-        primaryStage.setScene(scene);
-        primaryStage.show();
+		primaryStage.setScene(scene);
+		primaryStage.show();
     }
 	
 	public void setButtons(GridPane grid, PlayGame test) {   // nodes/buttons are going from top to down and then next column
-			Button btn1 = new Button("");    // (0, 0) coordinate on gameGrid
-	        Button btn2 = new Button("");    // (1, 0)
-	        Button btn3 = new Button("");    // (2, 0)
-	        Button btn4 = new Button("");    // (0, 1)
-	        Button btn5 = new Button("");    // (1, 1)
-	        Button btn6 = new Button("");    // (2, 1)
-	        Button btn7 = new Button("");    // (0, 2)
-	        Button btn8 = new Button("");    // (1, 2)
-	        Button btn9 = new Button("");    // (2, 2)
+		Button btn1 = new Button("");    // (0, 0) coordinate on gameGrid
+		Button btn2 = new Button("");    // (1, 0)
+		Button btn3 = new Button("");    // (2, 0)
+		Button btn4 = new Button("");    // (0, 1)
+		Button btn5 = new Button("");    // (1, 1)
+		Button btn6 = new Button("");    // (2, 1)
+		Button btn7 = new Button("");    // (0, 2)
+		Button btn8 = new Button("");    // (1, 2)
+		Button btn9 = new Button("");    // (2, 2)
 	        
-	        btn1.setMinSize(120, 120);
-	        btn2.setMinSize(120, 120);
-	        btn3.setMinSize(120, 120);
-	        btn4.setMinSize(120, 120);
-	        btn5.setMinSize(120, 120);
-	        btn6.setMinSize(120, 120);
-	        btn7.setMinSize(120, 120);
-	        btn8.setMinSize(120, 120);
-	        btn9.setMinSize(120, 120);
+		btn1.setMinSize(120, 120);
+		btn2.setMinSize(120, 120);
+		btn3.setMinSize(120, 120);
+		btn4.setMinSize(120, 120);
+		btn5.setMinSize(120, 120);
+		btn6.setMinSize(120, 120);
+		btn7.setMinSize(120, 120);
+		btn8.setMinSize(120, 120);
+		btn9.setMinSize(120, 120);
 	        
-	        btn1.setFont(Font.font("Tahoma", FontWeight.EXTRA_BOLD, 40));
-	        btn2.setFont(Font.font("Tahoma", FontWeight.EXTRA_BOLD, 40));
-	        btn3.setFont(Font.font("Tahoma", FontWeight.EXTRA_BOLD, 40));
-	        btn4.setFont(Font.font("Tahoma", FontWeight.EXTRA_BOLD, 40));
-	        btn5.setFont(Font.font("Tahoma", FontWeight.EXTRA_BOLD, 40));
-	        btn6.setFont(Font.font("Tahoma", FontWeight.EXTRA_BOLD, 40));
-	        btn7.setFont(Font.font("Tahoma", FontWeight.EXTRA_BOLD, 40));
-	        btn8.setFont(Font.font("Tahoma", FontWeight.EXTRA_BOLD, 40));
-	        btn9.setFont(Font.font("Tahoma", FontWeight.EXTRA_BOLD, 40));
+		btn1.setFont(Font.font("Tahoma", FontWeight.EXTRA_BOLD, 40));
+		btn2.setFont(Font.font("Tahoma", FontWeight.EXTRA_BOLD, 40));
+		btn3.setFont(Font.font("Tahoma", FontWeight.EXTRA_BOLD, 40));
+		btn4.setFont(Font.font("Tahoma", FontWeight.EXTRA_BOLD, 40));
+		btn5.setFont(Font.font("Tahoma", FontWeight.EXTRA_BOLD, 40));
+		btn6.setFont(Font.font("Tahoma", FontWeight.EXTRA_BOLD, 40));
+		btn7.setFont(Font.font("Tahoma", FontWeight.EXTRA_BOLD, 40));
+		btn8.setFont(Font.font("Tahoma", FontWeight.EXTRA_BOLD, 40));
+		btn9.setFont(Font.font("Tahoma", FontWeight.EXTRA_BOLD, 40));
 	        
-	        btn1.setTextFill(Color.DARKORANGE);
-	        btn2.setTextFill(Color.DARKORANGE);
-	        btn3.setTextFill(Color.DARKORANGE);
-	        btn4.setTextFill(Color.DARKORANGE);
-	        btn5.setTextFill(Color.DARKORANGE);
-	        btn6.setTextFill(Color.DARKORANGE);
-	        btn7.setTextFill(Color.DARKORANGE);
-	        btn8.setTextFill(Color.DARKORANGE);
-	        btn9.setTextFill(Color.DARKORANGE);
+		btn1.setTextFill(Color.DARKORANGE);
+		btn2.setTextFill(Color.DARKORANGE);
+		btn3.setTextFill(Color.DARKORANGE);
+		btn4.setTextFill(Color.DARKORANGE);
+		btn5.setTextFill(Color.DARKORANGE);
+		btn6.setTextFill(Color.DARKORANGE);
+		btn7.setTextFill(Color.DARKORANGE);
+		btn8.setTextFill(Color.DARKORANGE);
+		btn9.setTextFill(Color.DARKORANGE);
 	     
-	        // when setting buttons as disabled by setDisable(true) method (in setOnAction() method)
-	        // causes the text on the button to be very pale;
-	        // setting opacity by setOpacity() method causes the text to be well visible again 
-	        // (all values gives the same effect except 0)      
-	        btn1.setOpacity(50);     
-	        btn2.setOpacity(50); 
-	        btn3.setOpacity(50); 
-	        btn4.setOpacity(50); 
-	        btn5.setOpacity(50); 
-	        btn6.setOpacity(50); 
-	        btn7.setOpacity(50); 
-	        btn8.setOpacity(50); 
-	        btn9.setOpacity(50); 
+		// when setting buttons as disabled by setDisable(true) method (in setOnAction() method)
+		// causes the text on the button to be very pale;
+		// setting opacity by setOpacity() method causes the text to be well visible again 
+		// (all values gives the same effect except 0)      
+		btn1.setOpacity(50);     
+		btn2.setOpacity(50); 
+		btn3.setOpacity(50); 
+		btn4.setOpacity(50); 
+		btn5.setOpacity(50); 
+		btn6.setOpacity(50); 
+		btn7.setOpacity(50); 
+		btn8.setOpacity(50); 
+		btn9.setOpacity(50); 
 	        
-	        grid.add(btn1, 1, 2);
-	        grid.add(btn2, 1, 3);
-	        grid.add(btn3, 1, 4);
-	        grid.add(btn4, 2, 2);
-	        grid.add(btn5, 2, 3);
-	        grid.add(btn6, 2, 4);
-	        grid.add(btn7, 3, 2);
-	        grid.add(btn8, 3, 3);
-	        grid.add(btn9, 3, 4); 	
+		grid.add(btn1, 1, 2);
+		grid.add(btn2, 1, 3);
+		grid.add(btn3, 1, 4);
+		grid.add(btn4, 2, 2);
+		grid.add(btn5, 2, 3);
+		grid.add(btn6, 2, 4);
+		grid.add(btn7, 3, 2);
+		grid.add(btn8, 3, 3);
+		grid.add(btn9, 3, 4); 	
 	        
 	        
 	        btn1.setOnAction((t) -> {
--- src/ticTacToe2/GameInterface.java
+++ src/ticTacToe2/GameInterface.java
@@ -31,88 +31,88 @@ public class GameInterface extends Application {
 
 		PlayGame test = new PlayGame();
 		
-        primaryStage.setTitle("Tic Tac Toe");
+		primaryStage.setTitle("Tic Tac Toe");
 
-        GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(1);
-        grid.setVgap(1);
-        grid.setPadding(new Insets(35, 35, 35, 35));
-        grid.setStyle("-fx-background-color: transparent;");   // or null; if not set, scene background is (default) white always
-  //    grid.setGridLinesVisible(true);
+		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.CENTER);
+		grid.setHgap(1);
+		grid.setVgap(1);
+		grid.setPadding(new Insets(35, 35, 35, 35));
+		grid.setStyle("-fx-background-color: transparent;");   // or null; if not set, scene background is (default) white always
+	//	grid.setGridLinesVisible(true);
        
-        Text startingLine = new Text("Click me:)");
-        startingLine.setFill(Color.BLACK);
-        startingLine.setFont(Font.font("Tahoma", FontWeight.EXTRA_BOLD, 20));
-        grid.add(startingLine, 0, 0, 4, 1);
-        Label emptyLine = new Label("");
-        grid.add(emptyLine, 0, 1, 4, 1);
+		Text startingLine = new Text("Click me:)");
+		startingLine.setFill(Color.BLACK);
+		startingLine.setFont(Font.font("Tahoma", FontWeight.EXTRA_BOLD, 20));
+		grid.add(startingLine, 0, 0, 4, 1);
+		Label emptyLine = new Label("");
+		grid.add(emptyLine, 0, 1, 4, 1);
         
-        setButtons(grid, test);        
+		setButtons(grid, test);        
         
-        Label gameInfo = new Label("Game info: ");
-        gameInfo.setFont(Font.font("Tahoma", FontWeight.BOLD, 20));
-        grid.add(gameInfo, 1, 30, 2, 30);
+		Label gameInfo = new Label("Game info: ");
+		gameInfo.setFont(Font.font("Tahoma", FontWeight.BOLD, 20));
+		grid.add(gameInfo, 1, 30, 2, 30);
       
-        gameStatus = new Text("Player 1 starts the game");
-        gameStatus.setFill(Color.BROWN);
-        gameStatus.setFont(Font.font("Tahoma", FontWeight.BOLD, 20));
-        grid.add(gameStatus, 2, 30, 4, 30);   
+		gameStatus = new Text("Player 1 starts the game");
+		gameStatus.setFill(Color.BROWN);
+		gameStatus.setFont(Font.font("Tahoma", FontWeight.BOLD, 20));
+		grid.add(gameStatus, 2, 30, 4, 30);   
         
-        Label gameName = new Label("TicTacToe");
-        gameName.setFont(Font.font(STYLESHEET_CASPIAN, FontPosture.ITALIC, 14));
-        Label gameVersion = new Label("ver. 2.0");
-        gameVersion.setFont(Font.font(STYLESHEET_CASPIAN, FontPosture.ITALIC, 14));
-        gameVersion.setAlignment(Pos.BASELINE_RIGHT);
-        Label authorName = new Label("by dream_tree");
-        authorName.setFont(Font.font(STYLESHEET_CASPIAN, FontPosture.ITALIC, 14));
-        authorName.setTextFill(Color.FIREBRICK);
-        grid.add(gameName, 4, 70, 30, 70);
-        grid.add(gameVersion, 4, 80, 30, 80);
-        grid.add(authorName, 4, 90, 30, 90);
+		Label gameName = new Label("TicTacToe");
+		gameName.setFont(Font.font(STYLESHEET_CASPIAN, FontPosture.ITALIC, 14));
+		Label gameVersion = new Label("ver. 2.0");
+		gameVersion.setFont(Font.font(STYLESHEET_CASPIAN, FontPosture.ITALIC, 14));
+		gameVersion.setAlignment(Pos.BASELINE_RIGHT);
+		Label authorName = new Label("by dream_tree");
+		authorName.setFont(Font.font(STYLESHEET_CASPIAN, FontPosture.ITALIC, 14));
+		authorName.setTextFill(Color.FIREBRICK);
+		grid.add(gameName, 4, 70, 30, 70);
+		grid.add(gameVersion, 4, 80, 30, 80);
+		grid.add(authorName, 4, 90, 30, 90);
         
-        DropShadow shadow = new DropShadow();
-        shadow.setRadius(40.0);
-        shadow.setColor(Color.RED);
-   //   shadow.setOffsetX(-7.0);
-   //   shadow.setOffsetY(-7.0);
+		DropShadow shadow = new DropShadow();
+		shadow.setRadius(40.0);
+		shadow.setColor(Color.RED);
+	//	shadow.setOffsetX(-7.0);
+	//	shadow.setOffsetY(-7.0);
         
-        Button playAgain = new Button("Play again");
-        playAgain.setFont(Font.font("TAHOMA", 15));
-        playAgain.setEffect(shadow);
-        Circle playAgainCircle = new Circle(1);    // must be any radius specified
-        playAgain.setMinSize(80, 80);              // must be used to set circle at all
-        playAgain.setShape(playAgainCircle); 
-        grid.add(playAgain, 35, 2);
-        playAgain.setOnAction(e -> start(primaryStage));
+		Button playAgain = new Button("Play again");
+		playAgain.setFont(Font.font("TAHOMA", 15));
+		playAgain.setEffect(shadow);
+		Circle playAgainCircle = new Circle(1);    // must be any radius specified
+		playAgain.setMinSize(80, 80);              // must be used to set circle at all
+		playAgain.setShape(playAgainCircle); 
+		grid.add(playAgain, 35, 2);
+		playAgain.setOnAction(e -> start(primaryStage));
         
-        Button readMe = new Button("Read me");
-        readMe.setFont(Font.font("TAHOMA", 15)); 
-        readMe.setEffect(shadow);
-        Circle readMeCircle = new Circle(1);
-        readMe.setMinSize(80, 80);
-        readMe.setShape(readMeCircle); 
-        grid.add(readMe, 35, 3);
-        readMe.setOnAction(f -> {
-        			Random random = new Random();
-        			gameStatus.setText(randomText(random));
-        			gameStatus.setFill(Color.LIMEGREEN);
-       		});    
+		Button readMe = new Button("Read me");
+		readMe.setFont(Font.font("TAHOMA", 15)); 
+		readMe.setEffect(shadow);
+		Circle readMeCircle = new Circle(1);
+		readMe.setMinSize(80, 80);
+		readMe.setShape(readMeCircle); 
+		grid.add(readMe, 35, 3);
+		readMe.setOnAction(f -> {
+				Random random = new Random();
+				gameStatus.setText(randomText(random));
+				gameStatus.setFill(Color.LIMEGREEN);
+			});    
         
-        Button exit = new Button("Exit game");
-        exit.setFont(Font.font("TAHOMA", 15));        
-        exit.setEffect(shadow);
-        Circle exitCircle = new Circle(1);
-        exit.setMinSize(80, 80);
-        exit.setShape(exitCircle);
-        grid.add(exit, 35, 4);
-        exit.setOnAction(g -> System.exit(0));     
+		Button exit = new Button("Exit game");
+		exit.setFont(Font.font("TAHOMA", 15));        
+		exit.setEffect(shadow);
+		Circle exitCircle = new Circle(1);
+		exit.setMinSize(80, 80);
+		exit.setShape(exitCircle);
+		grid.add(exit, 35, 4);
+		exit.setOnAction(g -> System.exit(0));     
          
-        Scene scene = new Scene(grid, 700, 650);
-        scene.setFill(Color.NAVAJOWHITE); 
+		Scene scene = new Scene(grid, 700, 650);
+		scene.setFill(Color.NAVAJOWHITE); 
         
-        primaryStage.setScene(scene);
-        primaryStage.show();
+		primaryStage.setScene(scene);
+		primaryStage.show();
     }
 	
 	/* (non-Javadoc) 
